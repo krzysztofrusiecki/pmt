817ea0c5bc303acd77416dc6885aaca464ed3af3
krzysztofrusiecki <krzysztofrusiecki97@gmail.com>
2023-01-03 23:35:35 +0100

    fix e2e

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected div", async ({ page }) => {
 	await page.goto("/");
-	expect(await page.textContent("div")).toContain("Home");
+	expect(await page.textContent("h1")).toContain("PMT: Boost Your Team's Productivity");
 });
